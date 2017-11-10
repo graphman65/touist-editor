a5ce16fb231370e91a5d3db09a8dea87953fb97c
Maël Valais <mael.valais@gmail.com>
2017-11-10 19:14:05 +0100

    touist-server: cache dependencies with a nice trick (warning: in debug mode!)
    Thanks http://stackoverflow.com/questions/39661138/optimising-cargo-build-times-in-docker

---
+++ touist-server/dummy.rs
@@ -0,0 +1,3 @@
+// This dummy file is here only in order to cache the dependencies in docker,
+// because cargo build --libs needs a lib to be built.
+// See: https://stackoverflow.com/a/39662015/3808537
