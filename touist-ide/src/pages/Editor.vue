69afaf12d9c58d5b760e2248c3c0db02522a92e7
Sebastien Lacoste <contact@graphman.fr>
2017-11-10 22:47:50 +0100

    Add file list toggle

--- touist-ide/src/pages/Editor.vue
+++ touist-ide/src/pages/Editor.vue
@@ -2,8 +2,14 @@
   <div class="editor-page">
     <nav class="file-list-container" :class="{ open: fileListOpen }">
       <FileList />
+      <div
+        :style="{ left: fileListOpen ?  '250px' : '0px' }"
+        @click="fileListOpen = !fileListOpen"
+        class="file-list-toggle" :class="{  open: fileListToggleOpen }">
+        <i class="fa" :class="{ 'fa-chevron-left': fileListOpen, 'fa-chevron-right': !fileListOpen }"></i>
+      </div>
     </nav>
-    <main>
+    <main :style="{ width: fileListOpen ? 'calc(100% - 250px)' : '100%'}">
       <section class="editor-container">
         <TouistEditor @openModelList="modelListOpen = true" />
       </section>
@@ -34,6 +40,7 @@ export default {
   },
   data: () => ({
     fileListOpen: true,
+    fileListToggleOpen: true,
     modelListOpen: false,
   }),
   created() {
@@ -46,6 +53,9 @@ export default {
         await this.solve(this.openFile.name);
         this.modelListOpen = true;
       }
+    });
+    document.addEventListener('mousemove', (e) => {
+      this.fileListToggleOpen = e.clientX <= (this.fileListOpen ? 300 : 50);
     });
   },
   computed: {
@@ -63,18 +73,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/variables.scss";
+
 .editor-page {
   display: flex;
   flex-direction: row;
   height: 100%;
 }
 
+.file-list-toggle {
+  height: 50px;
+  width: 25px;
+  padding: 2px;
+  display: flex;
+  align-items: center;
+  border-top-right-radius: 50px;
+  border-bottom-right-radius: 50px;
+  border-right: 1px solid $dark_color;
+  border-top: 1px solid $dark_color;
+  border-bottom: 1px solid $dark_color;
+  background-color: $mid_color;
+  position: absolute;
+  top: calc(50vh - 20px);
+  z-index: 1000;
+  opacity: 0;
+  transition: opacity 0.1s, left 0.2s ease;
+  cursor: pointer;
+
+  &.open {
+    opacity: 1;
+  }
+}
+
 .file-list-container {
   width: 0px;
+  transition: width 0.2s ease, min-width 0.3s ease;
 
   &.open {
     width: 250px;
-    min-width: 250px;
+    // min-width: 250px;
   }
 }
 
@@ -82,7 +119,7 @@ main {
   display: flex;
   flex-direction: row;
   height: 100%;
-  width: 100%;
+  transition: width 0.2s ease;
 }
 
 .editor-container {
